3da93dee6f05c32d6b65985daa789e984d6f1b89
KokiOkumura <cajon.pj09@gmail.com>
2023-04-11 23:04:12 +0900

    deviseのフラッシュメッセージ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/show'
+  get 'user/edit'
+  root to: 'home#top'
   get 'home/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
